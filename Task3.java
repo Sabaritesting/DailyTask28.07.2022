2f7d2a3a5fb2fe25148a9bcfac3bd06483f781f6
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-29 13:12:32 +0530

    Add files via upload

---
+++ Task3.java
@@ -0,0 +1,13 @@
+package Task1;
+
+public class Task3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String name="javapoint tutitorial";
+System.out.println("Original String:"+name);
+int a=name.codePointCount(1, 10);
+System.out.println("Codepointcount "+a);
+	}
+
+}
