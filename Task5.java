ea5438b0deb55bcfbffd3f7c633edf3354bc04dc
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-29 13:13:06 +0530

    Add files via upload

---
+++ Task5.java
@@ -0,0 +1,15 @@
+package Task1;
+
+public class Task5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String name="Maven";
+String name1="demo";
+System.out.println(name);
+System.out.println(name1);
+String result=name.concat(name1);
+System.out.println(result);
+	}
+
+}
