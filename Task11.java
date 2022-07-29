bb3554f01ba311ee9fdb3d8b030086e1fa47cee9
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-29 13:16:02 +0530

    Add files via upload

---
+++ Task11.java
@@ -0,0 +1,60 @@
+package Task1;
+
+public class Task11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String level="The quick brown fox jumps over the lazy dog.";
+int a=level.indexOf("a",0);
+int b=level.indexOf("b",0);
+int c=level.indexOf("c",0);
+int d=level.indexOf("d",0);
+int e=level.indexOf("e",0);
+int f=level.indexOf("f",0);
+int g=level.indexOf("g",0);
+int h=level.indexOf("h",0);
+int i=level.indexOf("i",0);
+int j=level.indexOf("j",0);
+int k=level.indexOf("k",0);
+int l=level.indexOf("l",0);
+int m=level.indexOf("m",0);
+int n=level.indexOf("n",0);
+int o=level.indexOf("o",0);
+int p=level.indexOf("p",0);
+int q=level.indexOf("q",0);
+int r=level.indexOf("r",0);
+int s=level.indexOf("s",0);
+int t=level.indexOf("t",0);
+int u=level.indexOf("u",0);
+int v=level.indexOf("v",0);
+int w=level.indexOf("w",0);
+int x=level.indexOf("x",0);
+int y=level.indexOf("y",0);
+int z=level.indexOf("z",0);
+System.out.println(" a  b c  d e  f  g h i  j");
+System.out.println("=========================");
+System.out.println(a + " " + b + " " + c + " " + d + " " +
+                   e + " " + f + " " + g + " " + h + " " +
+                   i + " " + j + "\n");
+
+System.out.println("k  l  m  n  o  p q  r  s  t");
+System.out.println("===========================");
+System.out.println(k + " " + l + " " + m + " " + n + " " +
+                   o + " " + p + " " + q + " " + r + " " +
+                   s + " " + t + "\n");
+System.out.println(" a  b c  d e  f  g h i  j");
+System.out.println("=========================");
+System.out.println(a + " " + b + " " + c + " " + d + " " +
+                   e + " " + f + " " + g + " " + h + " " +
+                   i + " " + j + "\n");
+
+System.out.println("k  l  m  n  o  p q  r  s  t");
+System.out.println("===========================");
+System.out.println(k + " " + l + " " + m + " " + n + " " +
+                   o + " " + p + " " + q + " " + r + " " +
+                   s + " " + t + "\n");
+
+
+	}
+
+}
