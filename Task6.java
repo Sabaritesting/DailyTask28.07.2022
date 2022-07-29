879993cfe9a71c3b778a04c376442a60dc8c8d1c
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-29 13:13:25 +0530

    Add files via upload

---
+++ Task6.java
@@ -0,0 +1,14 @@
+package Task1;
+
+public class Task6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String give="Opinion for";
+String take="Opinion to";
+CharSequence cs = "Opinion";
+System.out.println("Comparing "+give+" and "+cs+": " + give.contentEquals(cs));
+System.out.println("Comparing "+take+" and "+cs+": " + take.contentEquals(cs));
+	}
+
+}
