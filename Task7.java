75fed64e5841f063f80dbe960f391525f7bbb557
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-29 13:13:50 +0530

    Add files via upload

---
+++ Task7.java
@@ -0,0 +1,12 @@
+package Task1;
+
+public class Task7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char[] arr_num = new char[] {'1','2','3','4'};
+		String name=String.copyValueOf(arr_num,1,3);
+		System.out.println("This chapter contains"+name+"pages");
+	}
+
+}
