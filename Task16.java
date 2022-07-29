4b3c9835036de444fe60a44228c0199806ad9fa5
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-29 13:18:24 +0530

    Add files via upload

---
+++ Task16.java
@@ -0,0 +1,13 @@
+package Task1;
+
+public class Task16 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s="StringExercises";
+String xs=s.trim();
+System.out.println("Original String:"+s);
+System.out.println("Modified String:"+xs);
+	}
+
+}
