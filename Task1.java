5b63845909bf0e14240e5910bdff4bbae9f7a567
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-29 13:11:00 +0530

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,15 @@
+package Task1;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String name="Hello SnowWorld";
+System.out.println("original string"+name);
+int a=name.charAt(2);
+int b=name.charAt(7);
+System.out.println("The character in position 2 is"+(char)+a);
+System.out.println("The character in positon 7 is "+(char)+b);
+	}
+
+}
