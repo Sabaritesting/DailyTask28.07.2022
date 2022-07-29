f89d9e81a30889f55179008b2b43a9737c562239
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-29 13:16:52 +0530

    Add files via upload

---
+++ Task13.java
@@ -0,0 +1,13 @@
+package Task1;
+
+public class Task13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String name="The Sky is blue";
+String newname=name.replace('s','b');
+System.out.println(name);
+System.out.println(newname);
+	}
+
+}
