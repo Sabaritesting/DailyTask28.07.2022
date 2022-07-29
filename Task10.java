11a37a0d599091fd0b873dfc91b51350f3eb95cb
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-29 13:15:37 +0530

    Add files via upload

---
+++ Task10.java
@@ -0,0 +1,12 @@
+package Task1;
+
+public class Task10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String name="Java Excersies ";
+int hash_code=name.hashCode();
+System.out.println("The hash code for"+name+"is"+hash_code);
+	}
+
+}
