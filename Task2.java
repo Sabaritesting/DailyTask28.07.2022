1dfabf9291d152bdcd22712324f163726debd4ab
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-29 13:11:17 +0530

    Add files via upload

---
+++ Task2.java
@@ -0,0 +1,15 @@
+package Task1;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String var="Boolean Figma JAvascript";
+System.out.println("Original String"+var);
+int a=var.codePointAt(2);
+int b=var.codePointAt(5);
+System.out.println("Unicode point"+a);
+System.out.println("Unicode point"+b);
+	}
+
+}
