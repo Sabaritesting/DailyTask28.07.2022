8331e468f1ab72ba1a8657eb280b024da3438616
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-29 13:17:40 +0530

    Add files via upload

---
+++ Task14.java
@@ -0,0 +1,16 @@
+package Task1;
+
+public class Task14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String name="Red is my favorite color";
+String name1="Orange is my favorite color";
+String color="Red";
+boolean starts1=name.startsWith(color);
+boolean starts2=name.startsWith(color);
+System.out.println(starts1);
+System.out.println(starts2);
+	}
+
+}
