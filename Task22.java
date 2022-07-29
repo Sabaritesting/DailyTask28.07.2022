d928bd1f8d7c2d352dacc3e71152dfe01a9cd496
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-29 13:21:24 +0530

    Add files via upload

---
+++ Task22.java
@@ -0,0 +1,17 @@
+package Task1;
+
+import java.util.Scanner;
+
+public class Task22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter first number");
+double a=sc.nextDouble();
+System.out.println("Enter second number");
+double b=sc.nextDouble();
+System.out.println(a>0&&a<1&&b>0&&b<1);
+	}
+
+}
