53fa54c63c802c656a4b27988c73bffaac806fb6
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-29 13:16:30 +0530

    Add files via upload

---
+++ Task12.java
@@ -0,0 +1,11 @@
+package Task1;
+
+public class Task12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String name="The sky is blue in color";
+int a=name.length();
+System.out.println("The length of the String is"+a);
+	}
+}
