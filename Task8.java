aa0d08e9599751b21ecf8f0f8fad1618713669ee
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-29 13:14:12 +0530

    Add files via upload

---
+++ Task8.java
@@ -0,0 +1,15 @@
+package Task1;
+
+import java.util.Calendar;
+
+public class Task8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Calendar t=Calendar.getInstance();
+System.out.println("Current Date and Time");
+System.out.format("%tB %te, %tY%n", t,t,t);
+System.out.format("%tl:%tM %tp%n", t,t,t);
+	}
+
+}
