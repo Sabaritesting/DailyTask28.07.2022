3011871d5bb9738099907464c6700b93d3179cf7
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-29 13:18:02 +0530

    Add files via upload

---
+++ Task15.java
@@ -0,0 +1,15 @@
+package Task1;
+
+public class Task15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String fir="the range of accuracy is low";
+String a=fir.toUpperCase();
+System.out.println(a);
+String vir="THE RANGE OF ACCURACY IS HIGH";
+String b=vir.toLowerCase();
+System.out.println(b);
+	}
+
+}
